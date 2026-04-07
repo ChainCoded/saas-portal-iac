83334bbc01a8ff8a22c919dbbf048fd7b49abd17
ChainCoded <garciamarcus3@yahoo.com>
2026-04-07 16:48:14 -0500

    added code_build role and policy

--- modules/cicd/main.tf
+++ modules/cicd/main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
 
 resource "aws_codebuild_project" "build" {
   name         = "${var.name_prefix}-build"
-  service_role = aws_iam_role.codepipeline_role.arn
+  service_role = aws_iam_role.codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -33,6 +33,26 @@ resource "aws_codebuild_project" "build" {
   source {
     type = "CODEPIPELINE"
   }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.name_prefix}-codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_codepipeline" "pipeline" {
